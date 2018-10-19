2a2d914c1508bd8cedec349145358831ca74c073
laolu <2295033476@qq.com>
2018-10-19 22:52:15 +0800

    第四次提交，版本4.0

--- example/src/cn/lpy/example/thread/TestThread.java
+++ example/src/cn/lpy/example/thread/TestThread.java
@@ -5,10 +5,15 @@ public class TestThread {
 	public static void main(String[] args) {
 		
 		say();
+		save();
 		System.out.println("我是克隆的");
 	}
 	
 	public static void say(){
 		System.out.println("参加活动！！！");
+	}
+	
+	public static void save(){
+		System.out.println("增加体重！！！");
 	}
 }
