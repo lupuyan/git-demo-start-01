9911423c84ed6f9122d4ea857405c41a553877e1
laolu <2295033476@qq.com>
2018-10-19 11:57:14 +0800

    从git中央仓库检出，并修改提交。版本3.0

--- example/src/cn/lpy/example/thread/TestThread.java
+++ example/src/cn/lpy/example/thread/TestThread.java
@@ -3,12 +3,9 @@ package cn.lpy.example.thread;
 public class TestThread {
 
 	public static void main(String[] args) {
-		MyThread th=new MyThread();
-		new Thread(th,"窗口一").start();
-		new Thread(th,"窗口二").start();
-		new Thread(th,"窗口三").start();
-		new Thread(th,"窗口四").start();
+		
 		say();
+		System.out.println("我是克隆的");
 	}
 	
 	public static void say(){
